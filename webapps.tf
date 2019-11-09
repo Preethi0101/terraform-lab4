2e74090045bd4f8b4956c972f431bdabbf01599a
Preethi0101 <44769985+Preethi0101@users.noreply.github.com>
2019-11-09 14:22:07 +0530

    added provider

--- webapps.tf
+++ webapps.tf
@@ -37,6 +37,14 @@ resource "azurerm_app_service" "citadel" {
     app_service_plan_id = "${element(azurerm_app_service_plan.free.*.id, count.index)}"
 }
 
+
+provider "azurerm" {
+  subscription_id = "32c32163-f026-41ed-9503-3b0ee6ec5540"
+  client_id       = "b9543670-34cb-410d-ad75-fb29e77964cc"
+  client_secret   = "Jg-xsz_p7Ru/5TOiRke_F1fsimnVUVJ8"
+  tenant_id       = "f66ccb58-5436-40de-ae8b-e951e03cb2b3"
+}
+
 output "webapp_ids" {
   value = "${azurerm_app_service.citadel.*.id}"
 }
